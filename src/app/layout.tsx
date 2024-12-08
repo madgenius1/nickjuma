51a19bc7f04372e068191c75584f5781918bbdff
MadGenius <nickjuma@outlook.com>
2024-12-09 01:45:50 +0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nick Juma",
+  description: "Portfolio For All Software Projects",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={montserrat.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex items-center justify-center py-12 bg-white">
+        <h2 className="text-center text-[#121212] text-2xl font-medium">About</h2>
+      </div>
+    </main>
+  );
+}

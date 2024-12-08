5c30b9fcfef065cbfa0784f48e44af6f794313ea
MadGenius <nickjuma@outlook.com>
2024-12-09 01:54:22 +0300

    Well, we are beginning again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center justify-center py-12 bg-white">
-        <h2 className="text-center text-[#121212] text-2xl font-medium">About</h2>
+        <h2 className="text-center text-[#121212] text-2xl font-medium">Nick Juma</h2>
       </div>
     </main>
   );

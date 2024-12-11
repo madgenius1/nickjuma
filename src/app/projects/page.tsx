37e4455a82316f39595ff104d8c83b0e449d50d6
MadGenius <nickjuma@outlook.com>
2024-12-11 18:40:31 +0300

    Projects Page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <main>
             <div className="py-12 flex justify-center items-center">
                 <h1 className="text-3xl text-[#4831D4] p-4 text-center">
-
+                    Projects Page
                 </h1>
             </div>
         </main>

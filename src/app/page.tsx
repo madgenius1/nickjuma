5f358ea557d39d1cb30cc2dd304f05d35dd3c93a
madgenius1 <nickjuma@outlook.com>
2026-01-15 17:05:34 +0300

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 
 import Hero from "./components/Hero";
 import About from "./components/About";
-// import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
@@ -16,7 +15,6 @@ export default function Home() {
       <About />
       <Work />
       <Experience />
-      {/* <Skills /> */}
       <Contact />
 
     </main>
